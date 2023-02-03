a688e951c8872878df971757a66a6d80e0b4281a
sueunal <doms@kakao.com>
2023-02-03 14:33:40 +0900

    switch문 추가

---
+++ Study/Myswitch.swift
@@ -0,0 +1,31 @@
+//
+//  Myswitch.swift
+//  Study
+//
+//  Created by sueun kim on 2023/02/03.
+//
+
+import SwiftUI
+
+struct Myswitch: View {
+    @State var menu :Menu = .beef
+    enum Menu {
+        case beef,pork,chicken
+    }
+    var body: some View {
+        switch menu{
+        case .beef,.chicken :
+            Text("오늘은 소고기")
+        case .pork:
+            Text("오늘은 돼지고기")
+        default:
+            Text("이 것은 못 만들어요")
+        }
+    }
+}
+
+struct Myswitch_Previews: PreviewProvider {
+    static var previews: some View {
+        Myswitch()
+    }
+}
