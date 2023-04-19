2d9af9d06f2258c4d594fd665071d8dabc1c4517
sueunal <36671600+sueunal@users.noreply.github.com>
2023-04-19 16:04:26 +0900

    [📖Study] : Divider 학습

---
+++ Study/level2/Level2_Divider.swift
@@ -0,0 +1,20 @@
+//
+//  Level2_Divider.swift
+//  Study
+//
+//  Created by sueun kim on 2023/04/19.
+//
+
+import SwiftUI
+
+struct Level2_Divider: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Level2_Divider_Previews: PreviewProvider {
+    static var previews: some View {
+        Level2_Divider()
+    }
+}
