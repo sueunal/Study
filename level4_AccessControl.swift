4ee54a53ae09da8e306aee06dedb8496fddd3e46
sueunal <36671600+sueunal@users.noreply.github.com>
2023-06-11 23:47:50 +0900

    [💡Create]: AccessControl 기능 추가
    기사님에 따라 alert 기능 추가

--- level4_AccessControl.swift
+++ level4_AccessControl.swift
@@ -4,8 +4,8 @@
 //
 //  Created by sueun kim on 2023/06/11.
 //
-
 import SwiftUI
+
 class Taxi{
     private var Driver : String // 누구나 변경하지 못하게 선언 보여지고 싶지 않을때 사용
     init(Driver:String) {
@@ -20,16 +20,26 @@ class Taxi{
 }
 struct level4_AccessControl: View {
     var myTaxi = Taxi(Driver: "Sueun")
+    @State private var alertToggle : Bool = false
+    @State private var DriverToggle : Bool = false
     var body: some View {
         VStack{
             Button{
-                print("현재 기사님은 \(myTaxi.getDriver()) 입니다 ")
+                print("현재 기사님은 \(myTaxi.getDriver()) 입니다")
+                alertToggle.toggle()
             }label: {
                 Text("Get Driver!")
+            }.alert(isPresented: $alertToggle) {
+                Alert(title:Text("현재 기사님은 \(myTaxi.getDriver()) 입니다 "))
             }
-            Button("DriverChange!"){
+            Button("DriverChanged!"){
                 myTaxi.ChangeDriver(NewDriver: "Leeo")
+                DriverToggle.toggle()
                 print("기사님을 변경하였습니다.")
+            }.alert(isPresented: $DriverToggle) {
+                Alert(title:Text("기사님이 변경되었습니다."),
+                      message: Text("변경된 기사님은 \(myTaxi.getDriver())님 입니다.")
+                )
             }
         }
     }
